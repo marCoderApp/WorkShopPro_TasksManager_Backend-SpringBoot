cba761c0af13e5a387fa3746d7ff682ee736d770
marcoderapp <farbusfer98@gmail.com>
2026-06-06 11:17:43 -0300

    First commit: Task manager project

---
+++ src/main/java/com/equipo/tallerproapp/TallerproappApplication.java
@@ -0,0 +1,13 @@
+package com.equipo.tallerproapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerproappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerproappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo/tallerproapp/TallerproappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.tallerproapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerproappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
